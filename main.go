1033227214dc679dd4460b864cd7b02cfd53a2fa
xiaosheng <2967625343@qq.com>
2023-09-30 21:10:33 +0800

    commit add: 布局调整

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"fmt"
-	"xiaosheng/ast"
-	"xiaosheng/tools"
 	"xiaosheng/views"
 
 	"fyne.io/fyne/v2"
@@ -12,16 +10,13 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"github.com/atotto/clipboard"
 )
-
-var listBox *widget.CheckGroup
 
 func main() {
 	myApp := app.New()
 	icon, _ := fyne.LoadResourceFromPath("main.ico")
 	myApp.SetIcon(icon)
-	myWindow := myApp.NewWindow("剪贴板监听器")
+	myWindow := myApp.NewWindow("小生 开发助手v0.3")
 
 	resultEntry := widget.NewEntry()
 	resultEntry.MultiLine = true
@@ -33,8 +28,8 @@ func main() {
 	})
 
 	// 创建一个标签
-	madeByLabel := widget.NewLabel("@xiaosheng ")
-	toolsLabel := widget.NewLabel("---小工具---")
+	madeByLabel := widget.NewLabel("	  @xiaosheng 	 ")
+	toolsLabel := widget.NewLabel("	   小工具")
 
 	// 将自定义的 TextStyle 应用到标签的 TextStyle 属性上
 	go views.StartClipboardListener(resultEntry)
@@ -48,8 +43,9 @@ func main() {
 		toolBtn,
 		madeByLabel,
 	)
+	content.Resize(fyne.NewSize(200, 300))
 
-	sqlParseContent := SqlContent(myApp, &myWindow)
+	sqlParseContent := views.SqlContent(myApp, &myWindow)
 
 	csqlbox := container.New(layout.NewVBoxLayout(), sqlParseContent)
 	csqlbox.Resize(fyne.NewSize(300, 300))
@@ -59,75 +55,4 @@ func main() {
 	myWindow.Content().Size().Max(fyne.NewSize(1920, 1000))
 	//myWindow.SetFixedSize(true)
 	myWindow.ShowAndRun()
-}
-
-func flushColumnsToListBox(myWindow fyne.Window) {
-	listBox.Options = tools.SqlColumns
-	myWindow.Content().Refresh()
-}
-
-// sql解析部分ui
-func SqlContent(myApp fyne.App, myWindow *fyne.Window) *fyne.Container {
-	// myWindow:=*myWindo
-	listBox = widget.NewCheckGroup([]string{" 111"}, func(selected []string) {
-		fmt.Println("Selected:", selected)
-		tools.SelectedRows = selected
-	})
-	//listBox.Hide()
-	listBoxContainer := container.NewVScroll(listBox) // Wrap the CheckGroup in a scrollable container
-	listBoxContainer.Resize(fyne.NewSize(200, 400))
-	listBoxContainer.SetMinSize(fyne.NewSize(200, 350))
-	listBoxContainer.Hide()
-	sqlParseContent := container.New(layout.NewVBoxLayout(),
-		widget.NewLabel("sql输入："),
-		container.NewHBox(widget.NewButton("输入 ", func() {
-			views.SqlParsePre(myApp)
-		}),
-			widget.NewButton("解析", func() {
-				ast.ParseSql(myApp)
-				fmt.Println("tools.column:", tools.SqlColumns)
-				flushColumnsToListBox(*myWindow)
-				//listBox.Show()
-				listBoxContainer.Show()
-				listBoxContainer.Refresh()
-				listBox.Refresh()
-			}),
-			widget.NewButton("生成", func() {
-				tw := myApp.NewWindow("target")
-				wet := widget.NewMultiLineEntry()
-				fmt.Println("tools.selected:", tools.SelectedRows)
-				element := tools.GetExcelElement(tools.SelectedRows)
-				wet.SetText(element)
-				wet.SetMinRowsVisible(20)
-				tools.SelectedText = element
-				box := container.NewVBox(wet, widget.NewButton("复制", func() {
-					clipboard.WriteAll(tools.SelectedText)
-					tw.Close()
-				}))
-				tw.SetContent(box)
-				tw.Resize(fyne.NewSize(300, 300))
-				tw.Show()
-			}),
-			widget.NewButton("clear", func() {
-				tools.SqlColumns = []string{}
-				tools.SqlStatement = ""
-				tools.SelectedRows = []string{}
-				// 清空 listBox 中的选定项
-				listBox.SetSelected([]string{})
-				flushColumnsToListBox(*myWindow)
-				listBox.Refresh()
-				listBoxContainer.Refresh()
-
-			}),
-			widget.NewCheck("是否追加", func(val bool) {
-				tools.IsAppended = val
-			}),
-		),
-		//listBoxContainer,
-		//listBox,
-	)
-	tmp := container.NewVBox(sqlParseContent, listBoxContainer)
-	tmp.Resize(fyne.NewSize(200, 400))
-	return tmp
-	//return sqlParseContent
 }
--- views/sql_parse.go
+++ views/sql_parse.go
@@ -6,8 +6,12 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/atotto/clipboard"
+	"xiaosheng/ast"
 	"xiaosheng/tools"
 )
+
+var listBox *widget.CheckGroup
 
 func SqlParsePre(myApp fyne.App) {
 	w3 := myApp.NewWindow("sql字串输入")
@@ -25,4 +29,74 @@ func SqlParsePre(myApp fyne.App) {
 	w3.SetContent(wec)
 	w3.Resize(fyne.NewSize(300, 400))
 	w3.Show()
+}
+
+// sql解析部分ui
+func SqlContent(myApp fyne.App, myWindow *fyne.Window) *fyne.Container {
+	// myWindow:=*myWindo
+	listBox = widget.NewCheckGroup([]string{" 111"}, func(selected []string) {
+		fmt.Println("Selected:", selected)
+		tools.SelectedRows = selected
+	})
+	//listBox.Hide()
+	listBoxContainer := container.NewVScroll(listBox) // Wrap the CheckGroup in a scrollable container
+	listBoxContainer.Resize(fyne.NewSize(200, 400))
+	listBoxContainer.SetMinSize(fyne.NewSize(200, 350))
+	listBoxContainer.Hide()
+	sqlParseContent := container.New(layout.NewVBoxLayout(),
+		widget.NewLabel("sql输入："),
+		container.NewHBox(widget.NewButton("输入 ", func() {
+			SqlParsePre(myApp)
+		}),
+			widget.NewButton("解析", func() {
+				ast.ParseSql(myApp)
+				fmt.Println("tools.column:", tools.SqlColumns)
+				flushColumnsToListBox(*myWindow)
+				//listBox.Show()
+				listBoxContainer.Show()
+				listBoxContainer.Refresh()
+				listBox.Refresh()
+			}),
+			widget.NewButton("生成", func() {
+				tw := myApp.NewWindow("target")
+				wet := widget.NewMultiLineEntry()
+				fmt.Println("tools.selected:", tools.SelectedRows)
+				element := tools.GetExcelElement(tools.SelectedRows)
+				wet.SetText(element)
+				wet.SetMinRowsVisible(20)
+				tools.SelectedText = element
+				box := container.NewVBox(wet, widget.NewButton("复制", func() {
+					clipboard.WriteAll(tools.SelectedText)
+					tw.Close()
+				}))
+				tw.SetContent(box)
+				tw.Resize(fyne.NewSize(300, 300))
+				tw.Show()
+			}),
+			widget.NewButton("clear", func() {
+				tools.SqlColumns = []string{}
+				tools.SqlStatement = ""
+				tools.SelectedRows = []string{}
+				// 清空 listBox 中的选定项
+				listBox.SetSelected([]string{})
+				flushColumnsToListBox(*myWindow)
+				listBox.Refresh()
+				listBoxContainer.Refresh()
+
+			}),
+			widget.NewCheck("是否追加", func(val bool) {
+				tools.IsAppended = val
+			}),
+		),
+		//listBoxContainer,
+		//listBox,
+	)
+	tmp := container.NewVBox(sqlParseContent, listBoxContainer)
+	tmp.Resize(fyne.NewSize(200, 400))
+	return tmp
+	//return sqlParseContent
+}
+func flushColumnsToListBox(myWindow fyne.Window) {
+	listBox.Options = tools.SqlColumns
+	myWindow.Content().Refresh()
 }
